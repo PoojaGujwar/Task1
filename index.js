659d073b2b3ad72b08293dcec6c2621635e3b39a
asthagujwar <gujwarastha@gmail.com>
2025-08-25 15:24:59 +0530

    todos get and post

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-const Task = require('./Task')
 
-const fileJson = fs.readFileSync(Task)
-console.log(fileJson)
+let todos =[]
+let id = 1
 
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
-console.log(Task)
-app.get("/v1/tasks",async(req,res)=>{
+console.log(todos)
+app.get("/v1/todos",async(req,res)=>{
     try {
-        const tasks = await Task.find()
-        console.log(tasks)
-        res.json(tasks)
+        res.json(todos)
     } catch (error) {
         res.status(500).json({error:"Internal Server error"})
     }
 })
+app.post("/v1/todos",async(req,res)=>{
+    const { todo } = req.body;
+    try{
+        const newTodo = new todos({id:id+1,todo})
+         todos.push(newTodo);
+        res.status(201).json(newTodo);
 
+    }catch(error){
+        res.status(500).json({error:"Internal Server error"})
+    }
+})
 const PORT = 3000
 app.listen(PORT,()=>{
 console.log(`Server is running on ${PORT}`)
